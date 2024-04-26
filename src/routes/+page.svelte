f0156a61a3eeb3ecd1d8ecc101238c7d646146ac
PillowGit <goldentaco727@gmail.com>
2024-04-26 12:06:16 -0700

    algo doing a sick backflip

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+
+</script>
+
+<div class="page-content">
+  <img src="icons/algo-logo.svg" alt="algo-logo"/>
+</div>
+
+<style>
+  .page-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: column nowrap;
+  }
+  .page-content>img {
+    margin-top: 10%;
+    width: 20%;
+    aspect-ratio: 1 / 1;
+
+    animation: sillylogo 2s infinite linear;
+    animation-delay: 1s;
+  }
+  @keyframes sillylogo {
+    0% {
+      transform: none;
+    }
+    40% {
+      transform: rotateX(180deg) translateY(-20rem);
+    }
+    80% {
+      transform: rotateX(360deg);
+    }
+    100% {
+      transform: rotateX(360deg) scaleX(-1);
+    }
+  }
+</style>
